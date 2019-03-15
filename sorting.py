b18f9a9c4b57bda99912636de7192f688a9dde14
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2019-03-15 18:55:55 +0530

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -79,11 +79,10 @@ halves and then merges the two sorted halves. The merge() function is used for m
 
 2. Merge sort is a perfectly elegant example of a Divide and Conquer algorithm. It simple uses the 2 main steps of such an algorithm:
 
-==> Continuously divide the unsorted list until you have N sublists, where each sublist has 1 element that is “unsorted” and N is 
-the number of elements in the original array.
-==> Repeatedly merge i.e conquer the sublists together 2 at a time to produce new sorted sublists until all elements have been 
-fully merged into a single sorted array.
-
+==> Continuously divide the unsorted list until you have N sublists, where each sublist has 1 element that is “unsorted” and N is the 
+number of elements in the original array.
+==> Repeatedly merge i.e conquer the sublists together 2 at a time to produce new sorted sublists until all elements have been fully 
+merged into a single sorted array.
 
 def mergeSort(alist):
     #print("Splitting ",alist)
@@ -113,16 +112,96 @@ def mergeSort(alist):
             alist[k]=righthalf[j]
             j=j+1
             k=k+1
-            return alist
-
+        return alist
 alist = [54,26,93,17,77,31,44,55,20]
 mergeSort(alist)
 print(alist)
 #[17, 20, 26, 31, 44, 54, 55, 77, 93]
-
 
 =============================
 =============================
 3. Insert sort :
 =============================
 =============================
+Insertion sort is both faster and well-arguably more simplistic than both bubble sort and selection sort. Funny enough, it’s 
+how many people sort their cards when playing a card game! On each loop iteration, insertion sort removes one element from the array.
+It then finds the location where that element belongs within another sorted array and inserts it there. It repeats this process until
+no input elements remain.
+
+def insertion_sort(arr):
+    for i in range(len(arr)):
+        cursor = arr[i]
+        while i > 0 and arr[i - 1] > cursor:
+            arr[i] = arr[i - 1]
+            i = i - 1
+        arr[i] = cursor
+    return arr
+
+arr = [4,7,8,9,6,5]
+insertion_sort(arr)
+print(arr)
+#[4, 5, 6, 7, 8, 9]
+
+=============================
+=============================
+4. Shell sort :
+=============================
+=============================
+
+http://interactivepython.org/runestone/static/pythonds/SortSearch/TheShellSort.html
+
+ShellSort is mainly a variation of Insertion Sort. In insertion sort, we move elements only one position ahead. When an element has 
+to be moved far ahead, many movements are involved. The idea of shellSort is to allow exchange of far items. In shellSort, we make the 
+array h-sorted for a large value of h. We keep reducing the value of h until it becomes 1. An array is said to be h-sorted if all 
+sublists of every h’th element is sorted.
+
+
+def shellSort(arr):
+    # Start with a big gap, then reduce the gap
+    mid = len(arr) // 2
+    while mid > 0:
+        for i in range(mid, len(arr)):
+            temp = arr[i]
+            j = i
+            while j >= mid and arr[j - mid] > temp:
+                arr[j] = arr[j - mid]
+                j -= mid
+
+            arr[j] = temp
+        mid //= 2
+
+arr = [1,4,7,8,9,5,6,3]
+shellSort(arr)
+print(arr)
+#[1, 3, 4, 5, 6, 7, 8, 9]
+
+=============================
+=============================
+5. Selection sort :
+=============================
+=============================
+
+Selection sort is also quite simple but frequently outperforms bubble sort. If you are choosing between the two, it’s best to just 
+default right to selection sort. With Selection sort, we divide our input list / array into two parts: the sublist of items already 
+sorted and the sublist of items remaining to be sorted that make up the rest of the list. We first find the smallest element in the 
+unsorted sublist and place it at the end of the sorted sublist. Thus, we are continuously grabbing the smallest unsorted element and 
+placing it in sorted order in the sorted sublist. This process continues iteratively until the list is fully sorted.
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        minimum = i
+
+        for j in range(i + 1, len(arr)):
+            # Select the smallest value
+            if arr[j] < arr[minimum]:
+                minimum = j
+
+        # Place it at the front of the
+        # sorted end of the array
+        arr[minimum], arr[i] = arr[i], arr[minimum]
+
+    return arr
+arr = [1,4,7,5,8,9,3,2]
+selection_sort(arr)
+print(arr)
+#[1, 2, 3, 4, 5, 7, 8, 9]
