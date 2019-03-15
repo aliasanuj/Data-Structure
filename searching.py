b72aefd30340a4fef5bc73e36ecdfde393f837f0
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2019-03-15 19:09:40 +0530

    Update searching.py

--- searching.py
+++ searching.py
@@ -1,2 +1,34 @@
-searching :
-=================
+Searching :
+==============
+Searching for data stored in different data structures is a crucial part of pretty much every single
+application. There are many different algorithms available to utilize when searching, and each have different
+implementations and rely on different data structures to get the job done. Being able to choose a specific
+algorithm for a given task is a key skill for developers and can mean the difference between a fast,
+reliable and stable application and an application that crumbles from a simple request.
+
+1. Linear Search
+2. Binary Search
+3. Jump Search
+4. Fibonacci Search
+5. Exponential Search
+6. Interpolation Search
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
